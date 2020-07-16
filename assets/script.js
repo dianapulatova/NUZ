7bfac90ed5a43b92f0e2c26637bcc4ea89f058f7
unknown <theankitbagchi@gmail.com>
2020-07-16 12:13:50 -0400

    merge fail

--- assets/script.js
+++ assets/script.js
@@ -6,13 +6,3 @@
 // var author = $("#article-author")
 // var date = $("#article-date")
 // var article = $("#article-text")
-
-var APIKey= "6d301cb460de4af881b497a471418db4"
-var queryURL= "http://newsapi.org/v2/everything?q=bitcoin&from=2020-06-15&sortBy=publishedAt&apiKey=" + APIKey
-
-$.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-      console.log(response);
-  })
