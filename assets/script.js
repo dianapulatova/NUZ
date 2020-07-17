87da5be8a9e5da5146f2f542aa38ca6cc4e8754f
Justin Ramirez <ramirezbjustin@gmail.com>
2020-07-17 13:15:00 -0400

    added new diffBot and Meaning Cloud API functionality within JS file

--- assets/script.js
+++ assets/script.js
@@ -9,75 +9,52 @@ var article = $("#article-text");
 
 $("#btn").on("click", function (event) {
   event.preventDefault();
+
   // Assigning user input to a var
   var inputUrl = $("#input-text").val();
+
   // trimming whitespaces off the url
   inputUrl = inputUrl.trim();
+
   // Diffbot Article API link
-  // inputUrl = inputUrl.replace(/:/gi, "%3A");
-  // inputUrl = inputUrl.replace(/\//gi, "%2A");
   inputUrl = encodeURIComponent(inputUrl);
   console.log(inputUrl);
-  var queryURL = `https://api.diffbot.com/v3/article?token=07098c9aee4fa512825707b13860eed2&url=${inputUrl}`;
+
+  // DiffBot API will be pulling text from websites and sentiment analysis
+  var queryURL = `https://api.diffbot.com/v3/article?token=07098c9aee4fa512825707b13860eed2&url=${inputUrl}&textAnalysis`;
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
+    console.log("diffbot = ", response);
+
+    // Article classes that will push reponse text on to page
     $(".article-title").text(response.objects[0].title);
     $(".article-source").text(response.objects[0].siteName);
     $(".article-author").text(response.objects[0].author);
     $(".article-date").text(response.objects[0].estimatedDate);
     $(".article-text").text(response.objects[0].text);
+    $(".sentiment-score").text(response.objects[0].sentiment);
+
+    // Text Summarizer - pushes the info of the diffbot api into the meaning cloud api
+    var text = response.objects[0].text;
+
+    // Meaning Cloud API
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: `https://meaningcloud-summarization-v1.p.rapidapi.com/summarization-1.0?txt=${text}&sentences=5`,
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "meaningcloud-summarization-v1.p.rapidapi.com",
+        "x-rapidapi-key": "4ac9883363mshfcfeac036daf56fp143340jsn21e45046a9c6",
+        accept: "application/json",
+      },
+    };
+    // This is adding the summary from the specific url into the summary section on the website
+    $.ajax(settings).done(function (summary) {
+      console.log(summary.summary);
+      $("#summary-text").text(summary.summary);
+    });
   });
-});
-
-// var arr = userInput.split(/(\s+)/).filter(function(e){return e.trim().length > 0;});;
-
-// var title = arr.join("%20");
-// console.log(title);
-// var proxyUrl = "52.179.231.206"
-// var queryURL= `${proxyUrl}http://newsapi.org/v2/everything?qInTitle=${title}&apiKey=6d301cb460de4af881b497a471418db4`
-// $.ajax({
-//     url: queryURL,
-//     method: "GET",
-//   }).then(function (response) {
-//       console.log(response);
-//   })
-// var title = $("#article-title")
-// var header = $("#arcticle-header")
-// var source = $("#article-source")
-// var container = $("#article-container")
-// var author = $("#article-author")
-// var date = $("#article-date")
-// var article = $("#article-text")
-
-var APIKey = "56949a1e-1226-4cc0-a36d-0ed86e97d690";
-var queryInURL =
-  "https://api.intellexer.com/intellexerApi/analyzeSentiments/?" + APIKey;
-
-$.ajax({
-  url: queryInURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-});
-
-var settings = {
-  async: true,
-  crossDomain: true,
-  url:
-    "http://api.intellexer.com/analyzeSentiments?apikey=YourAPIKey&ontology=hotels&loadSentences=true",
-  method: "POST",
-  headers: {
-    "content-type": "application/json",
-    "cache-control": "no-cache",
-  },
-  processData: false,
-  data:
-    '[{"id": "snt1", "text": "YourText"}, {"id": "snt2", "text": "YourText"}]',
-};
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
 });
